8d9248da733157a7a66b2b44d9c01655c5f1376b
Student <oscar.lopez102004.ol@gmail.com>
2025-09-26 11:43:11 -0700

    I added an option if you wanted the sandwich to be loaded.

--- workboobk1d/Sandwichshop/src/main/java/com/pluarlsight/Sandwichshop.java
+++ workboobk1d/Sandwichshop/src/main/java/com/pluarlsight/Sandwichshop.java
@@ -7,6 +7,8 @@ public class Sandwichshop {
     public static void main(String[] args) {
         String regular = "regular";
         String large = "large";
+        String yes = "yes";
+        String no = "no";
 
         Scanner scanner = new Scanner(System.in);
 
@@ -15,7 +17,7 @@ public class Sandwichshop {
 
         double price = 0;
 
-        if(choice.equals(regular)) {
+        if (choice.equals(regular)) {
             price = 5.45;
             System.out.println("The subtotal will be $" + price);
         } else if (choice.equals(large)) {
@@ -24,29 +26,36 @@ public class Sandwichshop {
         } else
             System.out.println("Invalid option pick regular of large!");
 
+        System.out.println("Do you want your Sandwich loaded (yes or no): ");
+        String loaded = scanner.nextLine();
 
-
-          System.out.println("What is your age: ");
-            int age = scanner.nextInt();
-
-
-            if(age < 18 ) {
-                price = price - (price * .1);
-                System.out.println("Your new total is: $" + price);
-            } else if(age >= 65) {
-                price = price - (price * .2);
-                System.out.println("Your new total is: $" + price);
-            }   else
-                System.out.println("your grand total is: $" + price);
-            }
-
-            }
+        if (loaded.equals(yes) && choice.equals(regular)) {
+            price = price + 1;
+            System.out.println("The new price is: $" + price);
+        } else if (loaded.equals(yes) && choice.equals(large)) {
+            price = price + 1.75;
+            System.out.println("The new price is: $" + price);
+        } else
+            System.out.println("no change in price");
 
 
 
 
+        System.out.println("What is your age: ");
+        int age = scanner.nextInt();
 
+        if (age < 18) {
+            price = price - (price * 0.1);
+            System.out.println("Your new total is: $"  +price);
+        } else if (age >= 65) {
+            price = price - (price * 0.2);
+            System.out.println("Your new total is: $" + price);
+        } else {
+            System.out.println("Your new total is: $" +price);
+        }
 
-    
+        scanner.close();
+    }
 
+}
 
