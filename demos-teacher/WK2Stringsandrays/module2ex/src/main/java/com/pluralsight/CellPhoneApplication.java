1a90665b34e95f71e06c31a2d23fd5ef2422e429
Student <oscar.lopez102004.ol@gmail.com>
2025-10-01 13:10:16 -0700

    I finished on  the sixth exercise of workbook 2 "Cell Phone Service"

---
+++ demos-teacher/WK2Stringsandrays/module2ex/src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -0,0 +1,102 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class CellPhoneApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CellPhone phone = new CellPhone();
+
+        System.out.print("What is the serial number? ");
+        int serialNumber = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("What is the Model of the Phone: ");
+        String model = scanner.nextLine();
+
+        System.out.println("What is the phone carrier: ");
+        String carrier = scanner.nextLine();
+
+        System.out.println("What is The phone number: ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.println("Who is the owner of the phone: ");
+        String owner = scanner.nextLine();
+
+
+        phone.setSerialNumber(serialNumber);
+        phone.setModel(model);
+        phone.setCarrier(carrier);
+        phone.setPhoneNumber(phoneNumber);
+        phone.setOwner(owner);
+
+        System.out.println("\nPhone Information:");
+        System.out.println("Serial Number: " + phone.getSerialNumber());
+        System.out.println("Phone Model: " + phone.getModel());
+        System.out.println("Carrier: " + phone.getCarrier());
+        System.out.println("Phone Number: " + phone.getPhoneNumber());
+        System.out.println("Owner: " + phone.getOwner());
+
+        scanner.close();
+
+
+    }
+}
+
+class CellPhone {
+    private int serialNumber;
+    private String model;
+    private String carrier;
+    private String phoneNumber;
+    private String owner;
+
+    public CellPhone() {
+        this.serialNumber = 0;
+        this.model = "";
+        this.carrier = "";
+        this.phoneNumber = "";
+        this.owner = "";
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    // Setters
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
