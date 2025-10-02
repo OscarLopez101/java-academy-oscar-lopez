f7d2260730c8ddb3d5de67e42aba879eda1d78f3
Student <oscar.lopez102004.ol@gmail.com>
2025-10-02 09:51:09 -0700

    Finish the module 3 exercises 1 "Looping"

---
+++ demos-teacher/WK2Stringsandrays/module3ex/src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println("I love Java");
+            i++;
+        }
+    }
+}
+
+class DoWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println("I Love Java");
+            i++;
+        } while (i <= 5);
+
+
+    }
+
+    }
